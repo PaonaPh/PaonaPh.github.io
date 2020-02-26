f9120962eb8e0168b1a445d8bd095bb102b8a443
Paona Sharma <paonasharma@gmail.com>
2020-02-26 22:56:18 +0530

    my-profile updated

---
+++ projects/My_Profile/script.js
@@ -0,0 +1,17 @@
+
+		var a=document.getElementById("dp1");
+        function settitle()
+        {a.title="paonasharma@gmail";}
+        function removetitle()
+        {
+        a.title="";
+        }
+        
+        function nextimg()
+        {
+        a.src ="./assets/image/dp2.jpg";
+        }
+        function previmg()
+        {
+        a.src ="./assets/image/dp1.jpeg";
+        }
